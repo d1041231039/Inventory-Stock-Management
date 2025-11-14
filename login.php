e54148426fd0a31da2ea059e1649c564adf2e5f5
Ferdyan <d1041231039@student.untan.ac.id>
2025-11-14 16:33:48 +0700

    Menghubungkan proses login dengan database untuk mencocokkan identitas pengguna.

---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+include 'koneksi.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM user WHERE Email='$email' AND Password='$password'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    session_start();
+    $user = $result->fetch_assoc();
+    $_SESSION['id_user'] = $user['ID_User'];
+    $_SESSION['username'] = $user['Username'];
+    header("Location: home.php");
+    exit();
+} else {
+    echo "<script>alert('Email atau password salah!'); window.history.back();</script>";
+    exit();
+}
+}
+
+$conn->close();
+?>
+
+<html>
+    <head>
+        <title>SIGMA</title>
+        <link rel="stylesheet" href="login.css">
+    </head>
+    <body>
+        <div class="container">
+            <p class="title">Login</p>
+            <form method="post" action="login.php">
+                <input class="input" name="email" type="text" placeholder="Enter your email" id="email" required>
+                <br>
+                <input class="input" name="password" type="password" placeholder="Enter your password" id="password" required>
+                <br>
+                <button class="button" type="submit">
+                    Login
+                </button>
+            </form>
+            <p class="additional">Don't have an account? <a href="register.php">Sign up</a></p>
+        </div>
+    </body>
+</html>
