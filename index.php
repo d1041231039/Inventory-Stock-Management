0168cfdb7e30df2ce2dc220028a7852559ae4762
Ferdyan <d1041231039@student.untan.ac.id>
2025-11-14 22:56:29 +0700

    Mengubah penamaan file dari home.php menjadi index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $recentQuery = $conn->query("SELECT * FROM stock WHERE ID_User=$id_user ORDER BY
 
 <head>
     <title>SIGMA - Home</title>
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
@@ -45,7 +45,7 @@ $recentQuery = $conn->query("SELECT * FROM stock WHERE ID_User=$id_user ORDER BY
                     fill="#1f1f1f">
                     <path
                         d="M120-520v-320h320v320H120Zm0 400v-320h320v320H120Zm400-400v-320h320v320H520Zm0 400v-320h320v320H520ZM200-600h160v-160H200v160Zm400 0h160v-160H600v160Zm0 400h160v-160H600v160Zm-400 0h160v-160H200v160Zm400-400Zm0 240Zm-240 0Zm0-240Z" />
-                </svg><a href="home.php">Dashboard</a></li>
+                </svg><a href="index.php">Dashboard</a></li>
             <li><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
                     fill="#1f1f1f">
                     <path
