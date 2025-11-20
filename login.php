9bf6fa5185d7ea0c050198aef0d17e05d7036b88
Ferdyan <d1041231039@student.untan.ac.id>
2025-11-20 17:14:16 +0700

    Mengganti query ke bentuk prepared statement untuk mencegah sql injection.

--- login.php
+++ login.php
@@ -5,10 +5,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM user WHERE Email='$email'";
-    $result = $conn->query($sql);
+    $stmt = $conn->prepare("SELECT * FROM user WHERE EMAIL = ?");
 
-    if ($result->num_rows > 0) {
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if($result->num_rows > 0) {
         $user = $result->fetch_assoc();
 
         if (password_verify($password, $user['Password'])) {
